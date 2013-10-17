1402d108fc787b0079d2a9be437d18373d1aee04
Álvaro Carrera <carrerabarroso@gmail.com>
2013-10-17 11:10:00 +0100

    Launcher to execute all experiment at once (useful for maven) :)

---
+++ src/main/java/es/upm/dit/gsi/barmas/solarflare/launcher/AllExperiments.java
@@ -0,0 +1,35 @@
+/**
+ * es.upm.dit.gsi.barmas.solarflare.launcher.AllExperiments.java
+ */
+package es.upm.dit.gsi.barmas.solarflare.launcher;
+
+import es.upm.dit.gsi.barmas.solarflare.launcher.experiments.Experiment1;
+import es.upm.dit.gsi.barmas.solarflare.launcher.experiments.Experiment2;
+
+/**
+ * Project: barmas
+ * File: es.upm.dit.gsi.barmas.solarflare.launcher.AllExperiments.java
+ * 
+ * Grupo de Sistemas Inteligentes
+ * Departamento de Ingeniería de Sistemas Telemáticos
+ * Universidad Politécnica de Madrid (UPM)
+ * 
+ * @author alvarocarrera
+ * @email a.carrera@gsi.dit.upm.es
+ * @twitter @alvarocarrera
+ * @date 17/10/2013
+ * @version 0.1
+ * 
+ */
+public class AllExperiments {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Experiment1.main(null);
+		Experiment2.main(null);
+	}
+
+}
