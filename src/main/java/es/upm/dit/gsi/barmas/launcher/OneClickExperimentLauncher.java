d325b9b2a83ce6ec4284dca8058724a8ea4ff4b2
Álvaro Carrera <carrerabarroso@gmail.com>
2013-11-18 19:37:00 +0000

    All experiments without kowlancz

--- src/main/java/es/upm/dit/gsi/barmas/launcher/OneClickExperimentLauncher.java
+++ src/main/java/es/upm/dit/gsi/barmas/launcher/OneClickExperimentLauncher.java
@@ -88,14 +88,14 @@ public class OneClickExperimentLauncher {
 	 */
 	private void click() {
 
-		// this.launchZooBatch();
-		// this.launchMarketingBatch();
-		// this.launchNurseryBatch();
-		// this.launchSolarFlareBatch();
-		this.launchKowlanCZBatch();
-		// this.launchMushroomBatch();
-		// this.launchChessBatch();
-		// this.launchPokerBatch();
+		this.launchZooBatch();
+		this.launchMarketingBatch();
+		this.launchNurseryBatch();
+		this.launchSolarFlareBatch();
+		// this.launchKowlanCZBatch();
+		this.launchMushroomBatch();
+		this.launchChessBatch();
+		this.launchPokerBatch();
 
 		long finishTime = System.currentTimeMillis();
 		long interval = finishTime - initTime;
