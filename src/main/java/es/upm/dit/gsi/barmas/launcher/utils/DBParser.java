d08408249397d24270718f8c3ae2ea9b8f056e76
Álvaro Carrera <carrerabarroso@gmail.com>
2014-02-10 17:29:28 +0100

    Ready for solarflare

--- src/main/java/es/upm/dit/gsi/barmas/launcher/utils/DBParser.java
+++ src/main/java/es/upm/dit/gsi/barmas/launcher/utils/DBParser.java
@@ -34,12 +34,12 @@ public class DBParser {
 //		String tableName = "zoo";
 //		String filePath = "../experiments/kowlancz02-simulation/kowlancz02-simulation-summary.csv";
 //		String tableName = "kowlancz02";
-//		String filePath = "../experiments/solarflare-simulation/solarflare-simulation-summary.csv";
-//		String tableName = "solarflare";
+		String filePath = "../experiments/solarflare-simulation/solarflare-simulation-summary.csv";
+		String tableName = "solarflare";
 //		String filePath = "../experiments/nursery-simulation/nursery-simulation-summary.csv";
 //		String tableName = "nursery";
-		String filePath = "../experiments/marketing-simulation/marketing-simulation-summary.csv";
-		String tableName = "marketing";
+//		String filePath = "../experiments/marketing-simulation/marketing-simulation-summary.csv";
+//		String tableName = "marketing";
 		dbparser.putDataInDB(tableName, filePath);
 
 	}// end main
