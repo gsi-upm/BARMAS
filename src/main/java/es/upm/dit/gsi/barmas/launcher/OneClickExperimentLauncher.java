c75974be0495ae1210ac3d023af6c279b45d2552
Álvaro Carrera <carrerabarroso@gmail.com>
2014-02-21 17:36:18 +0100

    Ready for solarflare2DB and marketing2simulation

--- src/main/java/es/upm/dit/gsi/barmas/launcher/OneClickExperimentLauncher.java
+++ src/main/java/es/upm/dit/gsi/barmas/launcher/OneClickExperimentLauncher.java
@@ -71,9 +71,9 @@ public class OneClickExperimentLauncher {
 	private void click() {
 
 		// this.launchZooBatch();
-		// this.launchMarketingBatch();
+		this.launchMarketingBatch();
 		// this.launchNurseryBatch();
-		this.launchSolarFlareBatch();
+		// this.launchSolarFlareBatch();
 		// this.launchKowlanCZBatch();
 		// this.launchMushroomBatch();
 		// this.launchChessBatch();
@@ -311,13 +311,13 @@ public class OneClickExperimentLauncher {
 		String classificationTarget = "Income";
 		double delta = 0.1;
 
-		double maxDistanceThreshold = 0.5;
-		double minDistanceThreshold = 0.1;
-		double maxBeliefThreshold = 0.5;
-		double minBeliefThreshold = 0.1;
-		double maxTrustThreshold = 0.5;
-		double minTrustThreshold = 0.1;
-		int maxLEBA = 7;
+		double maxDistanceThreshold = 0.2;
+		double minDistanceThreshold = 0.2;
+		double maxBeliefThreshold = 0.2;
+		double minBeliefThreshold = 0.2;
+		double maxTrustThreshold = 0.2;
+		double minTrustThreshold = 0.2;
+		int maxLEBA = 10;
 		int minLEBA = 0;
 		int maxArgumentationRounds = 200;
 		int maxNumberOfAgents = 5;
