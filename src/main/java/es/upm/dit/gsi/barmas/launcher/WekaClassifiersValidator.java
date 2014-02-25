00d3f5215128fd2d46e6073c03c9cf79f0618ec0
Álvaro Carrera <carrerabarroso@gmail.com>
2014-02-25 09:55:44 +0100

    Weka validator only with evaluation table algorithms

--- src/main/java/es/upm/dit/gsi/barmas/launcher/WekaClassifiersValidator.java
+++ src/main/java/es/upm/dit/gsi/barmas/launcher/WekaClassifiersValidator.java
@@ -14,27 +14,11 @@ import java.util.logging.Logger;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.functions.Logistic;
-import weka.classifiers.functions.MultilayerPerceptron;
-import weka.classifiers.functions.RBFNetwork;
 import weka.classifiers.functions.SMO;
-import weka.classifiers.functions.SimpleLogistic;
-import weka.classifiers.rules.ConjunctiveRule;
-import weka.classifiers.rules.DTNB;
-import weka.classifiers.rules.JRip;
-import weka.classifiers.rules.OneR;
 import weka.classifiers.rules.PART;
-import weka.classifiers.rules.ZeroR;
-import weka.classifiers.trees.BFTree;
-import weka.classifiers.trees.DecisionStump;
 import weka.classifiers.trees.J48;
-import weka.classifiers.trees.J48graft;
 import weka.classifiers.trees.LADTree;
-import weka.classifiers.trees.LMT;
 import weka.classifiers.trees.NBTree;
-import weka.classifiers.trees.REPTree;
-import weka.classifiers.trees.RandomForest;
-import weka.classifiers.trees.SimpleCart;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
@@ -77,13 +61,13 @@ public class WekaClassifiersValidator {
 	public static void main(String[] args) {
 
 		List<String> datasets = new ArrayList<String>();
-		datasets.add("zoo");
-		datasets.add("solarflare");
-		datasets.add("marketing");
-		datasets.add("nursery");
-		datasets.add("mushroom");
-		datasets.add("chess");
+		// datasets.add("zoo");
+		// datasets.add("solarflare");
+		// datasets.add("marketing");
+		// datasets.add("nursery");
+		// datasets.add("mushroom");
 		datasets.add("kowlancz02");
+		datasets.add("chess");
 		datasets.add("poker");
 
 		List<Classifier> classifiers = null;
@@ -520,104 +504,82 @@ public class WekaClassifiersValidator {
 		((J48) classifier).setUnpruned(true);
 		classifiers.add(classifier);
 
-		// J48Graft
-		classifier = new J48graft();
-		((J48graft) classifier).setUnpruned(true);
-		classifiers.add(classifier);
+		// // J48Graft
+		// classifier = new J48graft();
+		// ((J48graft) classifier).setUnpruned(true);
+		// classifiers.add(classifier);
 
-		// OneR
-		classifier = new OneR();
-		classifiers.add(classifier);
+		// // OneR
+		// classifier = new OneR();
+		// classifiers.add(classifier);
 
 		// LADTree
 		classifier = new LADTree();
 		classifiers.add(classifier);
 
-		// REPTree
-		classifier = new REPTree();
-		classifiers.add(classifier);
+		// // REPTree
+		// classifier = new REPTree();
+		// classifiers.add(classifier);
 
-		// SimpleLogistic
-		classifier = new SimpleLogistic();
-		classifiers.add(classifier);
+		// // SimpleLogistic
+		// classifier = new SimpleLogistic();
+		// classifiers.add(classifier);
 
-		// Logistic
-		classifier = new Logistic();
-		classifiers.add(classifier);
+		// // Logistic
+		// classifier = new Logistic();
+		// classifiers.add(classifier);
 
-		// MultiLayerPerceptron
-		classifier = new MultilayerPerceptron();
-		classifiers.add(classifier);
+		// // MultiLayerPerceptron
+		// classifier = new MultilayerPerceptron();
+		// classifiers.add(classifier);
 
-		// DecisionStump
-		classifier = new DecisionStump();
-		classifiers.add(classifier);
+		// // DecisionStump
+		// classifier = new DecisionStump();
+		// classifiers.add(classifier);
 
-		// LMT
-		classifier = new LMT();
-		classifiers.add(classifier);
+		// // LMT
+		// classifier = new LMT();
+		// classifiers.add(classifier);
 
-		// SimpleCart
-		classifier = new SimpleCart();
-		classifiers.add(classifier);
+		// // SimpleCart
+		// classifier = new SimpleCart();
+		// classifiers.add(classifier);
 
-		// BFTree
-		classifier = new BFTree();
-		classifiers.add(classifier);
+		// // BFTree
+		// classifier = new BFTree();
+		// classifiers.add(classifier);
 
-		// RBFNetwork
-		classifier = new RBFNetwork();
-		classifiers.add(classifier);
+		// // RBFNetwork
+		// classifier = new RBFNetwork();
+		// classifiers.add(classifier);
 
-		// DTNB
-		classifier = new DTNB();
-		classifiers.add(classifier);
+		// // DTNB
+		// classifier = new DTNB();
+		// classifiers.add(classifier);
 
-		// Jrip
-		classifier = new JRip();
-		classifiers.add(classifier);
+		// // Jrip
+		// classifier = new JRip();
+		// classifiers.add(classifier);
 
-		// Conjunction Rule
-		classifier = new ConjunctiveRule();
-		classifiers.add(classifier);
+		// // Conjunction Rule
+		// classifier = new ConjunctiveRule();
+		// classifiers.add(classifier);
 
-		// ZeroR
-		classifier = new ZeroR();
-		classifiers.add(classifier);
+		// // ZeroR
+		// classifier = new ZeroR();
+		// classifiers.add(classifier);
 
 		// SMO
 		classifier = new SMO();
 		classifiers.add(classifier);
-		
-		// NBTree
-		classifier = new NBTree();
-		classifiers.add(classifier);
 
-		// PART
-		classifier = new PART();
-		classifiers.add(classifier);
+		// // OneR
+		// classifier = new OneR();
+		// classifiers.add(classifier);
 
-		// RandomForest
-		classifier = new RandomForest();
-		classifiers.add(classifier);
-
-		// J48
-		classifier = new J48();
-		((J48) classifier).setUnpruned(true);
-		classifiers.add(classifier);
-
-		// J48Graft
-		classifier = new J48graft();
-		((J48graft) classifier).setUnpruned(true);
-		classifiers.add(classifier);
-
-		// OneR
-		classifier = new OneR();
-		classifiers.add(classifier);
-
-		// RandomForest
-		classifier = new RandomForest();
-		classifiers.add(classifier);
+		// // RandomForest
+		// classifier = new RandomForest();
+		// classifiers.add(classifier);
 
 		return classifiers;
 
